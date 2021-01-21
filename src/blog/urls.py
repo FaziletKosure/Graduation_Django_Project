4a390d40fea247efc88e8557ea0c1a2a7ed71f2f
FaziletKosure <faziletkosure1@gmail.com>
2021-01-21 21:41:53 +0100

    add

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,13 +1,3 @@
-# from django.urls import path
-# from .views import PostList, PostDetail, CommentList
-
-# app_name = 'blog'
-
-# urlpatterns = [
-#     path('<int:pk>/', PostDetail.as_view(), name='detailcreate'),
-#     path('', PostList.as_view(), name='listcreate'),
-#     path('comment/', CommentList.as_view(), name='comment-create'),
-# ]
 
 from .views import PostList, PostDetail, PostListDetailfilter, CreatePost, EditPost, AdminPostDetail, DeletePost
 from django.urls import path
